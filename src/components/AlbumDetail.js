76ef656e9e7a14ab18c028403408f8517719b473
Santiago Iraola <santiago.iraola@globant.com>
2018-03-23 12:51:06 -0300

    added different card sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ title }) => (
-  <Card>
-    <CardSection>
-      <Text style={{ fontSize: 20 }}>{ title }</Text>
-    </CardSection>
-  </Card>
-);
+const AlbumDetail = ({ title, artist, thumbnail_image, image }) => {
+  const { 
+    thumbnailStyle,
+    headerContentStyle, 
+    thumbnailContainerStyle,
+    headerFontSize,
+    imageStyle
+   } = styles;
 
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerFontSize}>{ title }</Text>
+          <Text>{ artist }</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+    </Card>
+  );
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerFontSize: {
+    fontSize: 20
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+}
 export default AlbumDetail;

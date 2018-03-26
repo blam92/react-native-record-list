b05f83008781154ca661c79b23a1fe2c9ecce4c1
Santiago Iraola <santiago.iraola@globant.com>
2018-03-26 13:00:56 -0300

    loginform handles press and renders err txt and spinner when waiting for res

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+
 export default class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    loginError: null,
+    loading: false
+  };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ loginError: null, loading: true });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((res) => {
+
+      })
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ loginError: 'Authentication failed.', loading: false });
+          });
+      });
+  }
 
   render() {
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
         <CardSection>
-          <Button title={'Sign In'} />
+          <Input 
+            label={'Email'} 
+            placeholder={'user@gmail.com'}
+            value={this.state.email}
+            onChangeText={(email) => this.setState({ email })}
+          />
+        </CardSection>
+        <CardSection>
+        <Input 
+          label={'Password'} 
+          placeholder={'password'}
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          secureText
+        />
+        </CardSection>
+        {this.state.loginError ? <Text style={styles.errorTextStyle}>{this.state.loginError}</Text> : null}
+        <CardSection>
+          {this.state.loading ? <Spinner /> : <Button style={styles.buttonStyle} title={'Sign In'} onPress={this.onButtonPress.bind(this)} />}
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  },
+  buttonStyle: {
+    flex: 1
+  }
+};
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { TouchableOpacity, Button as B } from 'react-native';
 
-const Button = ({ color, onPress, title }) => (
-    <TouchableOpacity>
+const Button = ({ color, onPress, title, style }) => (
+    <TouchableOpacity style={style}>
       <B color={color} title={title} onPress={onPress} />
     </TouchableOpacity>
 );

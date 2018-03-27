229a4f7be0b1e2f6ab7868f9fc546d3df3170567
Santiago Iraola <santiago.iraola@globant.com>
2018-03-27 15:44:29 -0300

    refactored bottomnav for reusability

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { ScrollView, View } from 'react-native';
 import AlbumDetail from './AlbumDetail';
-import { Button, BottomNav } from './common';
+import { BottomNav } from './common';
 
+const highlight = 'F0F4C3';
 class AlbumList extends Component {
   state = { 
     albums: []
@@ -16,7 +17,34 @@ class AlbumList extends Component {
     })
     .catch((err) => console.log(err));
   }
-  
+
+  navItems = {
+    labelColor: '#827717',
+    onTabChange: () => 2,
+    backgroundColor: '#CDDC39',
+    tabs: [
+      {
+        onPress: () => 2,
+        label: 'Music',
+        icon: 'music-note',
+        iconColor: '#827717'
+      },
+      {
+        onPress: () => 2,
+        label: 'Tech Stack',
+        icon: 'code',
+        iconColor: '#827717'
+      },
+      {
+        onPress: () => this.props.signOut,
+        label: 'Sign Out',
+        icon: 'power-settings-new',
+        iconColor: '#827717'
+      }
+    ]
+  };
+
+
   renderAlbums() {
     return this.state.albums.map((album) => <AlbumDetail key={album.title} {...album} />);
   }
@@ -24,11 +52,10 @@ class AlbumList extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Button title={'Log Out'} onPress={this.props.signOut} color={'#827717'} />
         <ScrollView>
           {this.renderAlbums()}
         </ScrollView>
-        <BottomNav />
+        <BottomNav {...this.navItems} />
       </View>
     );
   }
--- src/components/common/BottomNav.js
+++ src/components/common/BottomNav.js
@@ -2,11 +2,23 @@ import React from 'react';
 import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const BottomNav = () => {
+const BottomNav = ({ labelColor, onTabChange, backgroundColor, tabs }) => {
+
+  const tabComponents = tabs.map((tab) => {
+    return (
+      <Tab
+        key={tab.label}
+        onPress={tab.onPress}
+        barBackgroundColor={backgroundColor}
+        label={tab.label}
+        icon={<Icon size={24} color={tab.iconColor} name={tab.icon} />}
+      />
+    )
+  });
   return (
     <BottomNavigation
-    labelColor="#808080"
-    rippleColor="white"
+    labelColor={labelColor}
+    rippleColor={'#808080'}
     style={{
       height: 56,
       elevation: 8,
@@ -15,28 +27,9 @@ const BottomNav = () => {
       bottom: 0,
       right: 0
     }}
-    onTabChange={newTabIndex => alert(`New Tab at position ${newTabIndex}`)}
+    onTabChange={onTabChange}
   >
-    <Tab
-      barBackgroundColor="#CDDC39"
-      label="Movies & TV"
-      icon={<Icon size={24} color="#808080" name="tv" />}
-    />
-    <Tab
-      barBackgroundColor="#CDDC39"
-      label="Music"
-      icon={<Icon size={24} color="#808080" name="music-note" />}
-    />
-    <Tab
-      barBackgroundColor="#CDDC39"
-      label="Books"
-      icon={<Icon size={24} color="#808080" name="book" />}
-    />
-    <Tab
-      barBackgroundColor="#CDDC39"
-      label="Newsstand"
-      icon={<Icon size={24} color="#808080" name="assistant" />}
-    />
+  { tabComponents }
   </BottomNavigation>
   );
 }
